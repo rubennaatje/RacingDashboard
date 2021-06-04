30bee81d921bee825d13aa5f22d49782e9abad34
Ruben <ruben.soerdien@Hotmail.com>
2021-06-04 23:29:05 +0200

    empty gh-pages

--- plugins/leaflet.js
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue'
-import { LMap, LTileLayer, LMarker } from 'vue2-leaflet'
-
-Vue.component('LMap', LMap)
-Vue.component('LTileLayer', LTileLayer)
-Vue.component('LMarker', LMarker)
